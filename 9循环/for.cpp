0d8eb2a8f0c47cdcc79f12ba0c4f47ba5fc11221
coolcumt <cool_kj@126.com>
2018-07-31 17:29:44 +0800

    无限循环-while-for

---
+++ 9循环/for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    for(int a = 10; a < 20; a++)
+    {
+        cout << "a 的值: " << a << endl;
+    }
+    return 0;
+}
---
+++ 9循环/while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 10;
+
+    while(a < 20)
+    {
+        cout << "a 的值: " << a << endl;
+        a++;
+    }
+    return 0;
+}
