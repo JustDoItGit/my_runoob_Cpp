7a3f4d4b5a8f351d4167567fa16b0955308c42fd
coolcumt <cool_kj@126.com>
2018-08-08 11:02:40 +0800

    内联函数

--- 23友元函数/友元函数.cpp
+++ 23友元函数/友元函数.cpp
@@ -14,8 +14,10 @@ void Box::setWidth(double wid)
     width = wid;
 }
 
+// 请注意：printWidth() 不是任何类的成员函数
 void printWidth(Box box)
 {
+    /* 因为 printWidth() 是 Box 的友元，它可以直接访问该类的任何成员 */
     cout << "Width of box : " << box.width << endl;
 }
 
@@ -24,6 +26,7 @@ int main(int argc, char const *argv[])
     Box box;
 
     box.setWidth(10.0);
+    // 使用友元函数输出宽度
     printWidth(box);
     return 0;
 }
