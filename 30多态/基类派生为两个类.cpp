0426c77580ed07294a5c21bb2dcaaeb9f09bf5f3
coolcumt <cool_kj@126.com>
2018-08-11 20:37:09 +0800

    多态

---
+++ 30多态/基类派生为两个类.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+protected:
+    int width, height;
+public:
+    Shape(int a = 0, int b = 0)
+    {
+        width = a;
+        height = b;
+    }
+    virtual int area();
+};
+
+/*Shape::Shape(int a = 0, int b = 0)
+{
+    width = a;
+    height = b;
+}*/
+int Shape::area()
+{
+    cout << "Parent class area:" << endl;
+    return 0;
+}
+
+class Rectabgle: public Shape
+{
+public:
+    Rectabgle(int a = 0, int b = 0) : Shape(a, b) {};
+    int area();
+};
+
+int Rectabgle::area()
+{
+    cout << "Rectabgle class area:" << endl;
+    return width * height;
+}
+
+class Triangle: public Shape
+{
+public:
+    Triangle(int a = 0, int b = 0) : Shape(a, b) {};
+    int area();
+};
+
+int Triangle::area()
+{
+    cout << "Triangle class area:" << endl;
+    return width * height;
+}
+
+int main(int argc, char const *argv[])
+{
+    Shape *shape;
+    Rectabgle rec(10, 7);
+    Triangle tri(10, 5);
+
+    shape = &rec;
+    shape -> area();
+
+    shape = &tri;
+    shape -> area();
+
+    rec.area();
+    tri.area();
+    return 0;
+}
